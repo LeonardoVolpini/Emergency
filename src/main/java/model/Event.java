e2d92b1c5c56306962bf0d5cacc1ab5ba9f30b73
Leo Volpe <Leo Volpe@LAPTOP-VU9KPGVR>
2021-05-17 11:34:03 +0200

    lezione lunedi 17 maggio

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -1,5 +1,61 @@
 package model;
 
-public class Event {
+import java.time.LocalTime;
 
+public class Event implements Comparable<Event>{
+
+	public enum EventType{
+		ARRIVAL, //arrivo nuovo paziente, entra in triage
+		TRIAGE, // e' finito il triage, entro in sala d'attesa
+		TIMEOUT, // passa un certo tempo di attesa
+		FREE_STUDIO, // si e' liberato uno studio, chiamiamo qualche paziente
+		TREATED, //paziente curato
+		TICK //timer pre controllare se ci sono studi vuoti
+	};
+	
+	private LocalTime time; //marcatura temporale dell'evento stesso
+	private EventType type;
+	private Patient patient;
+	
+	public Event(LocalTime time, EventType type, Patient patient) {
+		this.time = time;
+		this.type = type;
+		this.patient = patient;
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
+
+	public EventType getType() {
+		return type;
+	}
+
+	public void setType(EventType type) {
+		this.type = type;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	@Override
+	public int compareTo(Event other) {
+		return this.time.compareTo(other.time);
+	}
+
+	@Override
+	public String toString() {
+		return "Event [time=" + time + ", type=" + type + ", patient=" + patient + "]";
+	}
+	
+	
 }
--- src/main/java/model/TestSimulator.java
+++ src/main/java/model/TestSimulator.java
@@ -3,8 +3,15 @@ package model;
 public class TestSimulator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Simulator sim = new Simulator();
+		sim.init();
+		sim.run();
 
+		System.out.println("Pazienti che hanno abbandonato: "+sim.getPatientsAbandoned());
+		System.out.println("Pazienti morti: "+sim.getPatientsDead());
+		System.out.println("Pazienti curati: "+sim.getPatientsTreated());
+		
 	}
 
 }
